e18304bf667eabfd3405f00563f4181c5fe7f18a
Peter Monks <pmonks@gmail.com>
2014-01-17 22:33:18 -0800

    Added multi-arity version of public-java-api.

--- src/alfresco_technical_validation/alfresco_public_api.clj
+++ src/alfresco_technical_validation/alfresco_public_api.clj
@@ -7,14 +7,18 @@
 ; Commons, 444 Castro Street, Suite 900, Mountain View, California, 94041, USA.
 ;
 
-(ns alfresco-technical-validation.alfresco-public-api)
+(ns alfresco-technical-validation.alfresco-public-api
+  (:require [clojure.tools.logging :as log]))
+
 
 (def ^:private ^String api-page-url       "http://docs.alfresco.com/4.2/topic/com.alfresco.enterprise.doc/concepts/java-public-api-list.html")
 (def ^:private ^String api-list-open-tag  "<pre class=\"pre codeblock\" xml:space=\"preserve\">")
 (def ^:private ^String api-list-close-tag "</pre>")
 
 (defn public-java-api
-  []
-  (let [api-page-html ^String (slurp api-page-url)]
-    (seq (.substring api-page-html (+ (.indexOf api-page-html api-list-open-tag) (.length api-list-open-tag))
-                                      (.indexOf api-page-html api-list-close-tag)))))
+  ([] public-java-api api-page-url)
+  ([url]
+   (log/debug "Retrieving Alfresco Public Java API list from" url "...")
+   (let [api-page-html ^String (slurp url)]
+     (seq (.substring api-page-html (+ (.indexOf api-page-html api-list-open-tag) (.length api-list-open-tag))
+                                       (.indexOf api-page-html api-list-close-tag))))))
