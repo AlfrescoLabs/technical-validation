87f3f1ec7364e7668d46fec6fbc998a7ad4bd9ca
Peter Monks <pmonks@gmail.com>
2014-02-13 23:34:05 -0800

    Refactored out standard validation logic into helper function. Added missing SEC05_Evidence bookmark to the template.

--- src/alfresco_technical_validation/source_validation.clj
+++ src/alfresco_technical_validation/source_validation.clj
@@ -18,21 +18,21 @@
 (def ^:private file-types
   "The file types we're interested in, with symbolic names and filename regex to identify them."
   {
-    :module-properties      #"module\.properties"
-    :java                   #".*\.java"
-    :javascript             #".*\.js"
-    :freemarker             #".*\.ftl"
-    :xml                    #".*\.xml"
-    :web-script-descriptors #".*\.desc\.xml"
-    :spring-app-context     #".*-context\.xml"
-    :content-model          #".*model.*\.xml"
-    :ant                    #"build\.xml"
-    :maven                  #"pom\.xml"
-    :gradle                 #"build\.gradle"
-    :leiningen              #"project\.clj"
-    :sbt                    #"build\.sbt"
-    :make                   #"[mM]akefile"
-    :pants                  #"BUILD"
+    :module-properties     #"module\.properties"
+    :java                  #".*\.java"
+    :javascript            #".*\.js"
+    :freemarker            #".*\.ftl"
+    :xml                   #".*\.xml"
+    :web-script-descriptor #".*\.desc\.xml"
+    :spring-app-context    #".*-context\.xml"
+    :content-model         #".*model.*\.xml"
+    :ant                   #"build\.xml"
+    :maven                 #"pom\.xml"
+    :gradle                #"build\.gradle"
+    :leiningen             #"project\.clj"
+    :sbt                   #"build\.sbt"
+    :make                  #"[mM]akefile"
+    :pants                 #"BUILD"
   })
 
 (defn- build-file-type-index
@@ -46,24 +46,32 @@
 (def ^:private content-regexes-by-file-type
   "Regexes we want to run over each file type."
   {
-    :module-properties  {
-                          :module-id      #"module\.id=(.*)\z"
-                          :module-version #"module\.version=(.*)\z"
-                          :repo-min       #"module\.repo\.version\.min=(.*)\z"
-                          :repo-max       #"module\.repo\.version\.max=(.*)\z"
-                          :alf-edition    #"module\.edition=(.*)\z"
-                        }
-    :ant                { :ivy            #"antlib:org\.apache\.ivy\.ant" }
-    :java               {
-                          :stb08-stb09    #"(?:^|\s)synchronized(?:\s|$)"
-                        }
+    :module-properties {
+        :module-id      #"module\.id=(.*)\z"
+        :module-version #"module\.version=(.*)\z"
+        :repo-min       #"module\.repo\.version\.min=(.*)\z"
+        :repo-max       #"module\.repo\.version\.max=(.*)\z"
+        :alf-edition    #"module\.edition=(.*)\z"
+      }
+    :java {
+        :stb08-stb09    #"(?:^|\s)synchronized(?:\s|$)"
+      }
+    :javascript {
+        :sec05          #"(?:^|\s)eval\("
+      }
+    :web-script-descriptor {
+        :sec03          #"<authentication>\s*none\s*</authentication>"
+      }
     :spring-app-context {
-                          :api05          #"(?:^|\s)ref="
-                        }
-    :content-model      {
-                          :perf02         #"<index enabled\s*=\s*\"true"
-                          :perf03         #"<stored>\s*true\s*</stored>"
-                        }
+        :api05          #"(?:^|\s)ref="
+      }
+    :content-model {
+        :perf02         #"<index enabled\s*=\s*\"true"
+        :perf03         #"<stored>\s*true\s*</stored>"
+      }
+    :ant {
+        :ivy            #"antlib:org\.apache\.ivy\.ant"
+      }
   })
 
 (defn- build-content-index-for-file-type
@@ -111,17 +119,27 @@
         message (s/join "," matches)]
     { "AlfrescoVersionMax" (if (empty? message) "Not specified" message) }))
 
-(defn- api05-inject-serviceregistry-not-services
-  [source content-index]
-  (let [matches (filter #(= :api05 (:regex-id %)) content-index)
-        message (str "Bean injections:\n"
+(defn- standard-validation
+  [source content-index regex-id criteria-id message-header manual-followup-required success-message]
+  (let [matches (filter #(= regex-id (:regex-id %)) content-index)
+        message (str message-header ":\n"
                      (s/join "\n"
                              (map #(str (subs (str (:file %)) (.length ^String source)) " line " (:line-number %) ": " (:line %))
                                   matches)))]
-      (build-bookmark-map "API05"
+      (build-bookmark-map criteria-id
                           (empty? matches)
-                          (str message "\n#### Manual followup required. ####")
-                          "The technology does not perform bean injection.")))
+                          (if manual-followup-required (str message "\n#### Manual followup required. ####") message)
+                          success-message)))
+
+(defn- api05-inject-serviceregistry-not-services
+  [source content-index]
+  (standard-validation source
+                       content-index
+                       :api05
+                       "API05"
+                       "Bean injections"
+                       true
+                       "The technology does not perform bean injection."))
 
 (defn- stb08-stb09-use-of-synchronized
   [source content-index]
@@ -142,27 +160,43 @@
 
 (defn- perf02-judicious-use-of-indexed-properties
   [source content-index]
-  (let [matches (filter #(= :perf02 (:regex-id %)) content-index)
-        message (str "Indexed content model properties:\n"
-                     (s/join "\n"
-                             (map #(str (subs (str (:file %)) (.length ^String source)) " line " (:line-number %) ": " (:line %))
-                                  matches)))]
-      (build-bookmark-map "PERF02"
-                          (empty? matches)
-                          (str message "\n#### Manual followup required. ####")
-                          "The technology does not index any content model properties.")))
+  (standard-validation source
+                       content-index
+                       :perf02
+                       "PERF02"
+                       "Indexed content model properties"
+                       true
+                       "The technology does not index any content model properties."))
 
 (defn- perf03-dont-store-property-values
   [source content-index]
-  (let [matches (filter #(= :perf03 (:regex-id %)) content-index)
-        message (str "Stored content model properties:\n"
-                     (s/join "\n"
-                             (map #(str (subs (str (:file %)) (.length ^String source)) " line " (:line-number %) ": " (:line %))
-                                  matches)))]
-      (build-bookmark-map "PERF03"
-                          (empty? matches)
-                          message
-                          "The technology does not store any content model properties in the search engine indexes.")))
+  (standard-validation source
+                       content-index
+                       :perf03
+                       "PERF03"
+                       "Stored content model properties"
+                       false
+                       "The technology does not store any content model properties in the search engine indexes."))
+
+(defn- sec03-none-authentication-in-web-scripts
+  [source content-index]
+  (standard-validation source
+                       content-index
+                       :sec03
+                       "SEC03"
+                       "'None' authentication in Web Scripts"
+                       false
+                       "The technology does not use 'none' authentication in Web Scripts."))
+
+(defn- sec05-use-of-eval
+  [source content-index]
+  (standard-validation source
+                       content-index
+                       :sec05
+                       "SEC05"
+                       "Uses of eval in Javascript"
+                       false
+                       "The technology does not use eval in Javascript."))
 
 (defn validate
   "Runs all source-based validations."
@@ -179,5 +213,7 @@
       (api05-inject-serviceregistry-not-services  source content-index)
       (perf02-judicious-use-of-indexed-properties source content-index)
       (perf03-dont-store-property-values          source content-index)
+      (sec03-none-authentication-in-web-scripts   source content-index)
+      (sec05-use-of-eval                          source content-index)
     )))
   
