30e4ce08817f99813d61ce4d542879fc262404f3
Peter Monks <pmonks@gmail.com>
2014-04-09 18:09:43 -0700

    Added missing files

---
+++ src/alfresco_technical_validation/indexer.clj
@@ -0,0 +1,69 @@
+;
+; Copyright © 2013,2014 Peter Monks (pmonks@gmail.com)
+;
+; Licensed under the Apache License, Version 2.0 (the "License");
+; you may not use this file except in compliance with the License.
+; You may obtain a copy of the License at
+; 
+;     http://www.apache.org/licenses/LICENSE-2.0
+; 
+; Unless required by applicable law or agreed to in writing, software
+; distributed under the License is distributed on an "AS IS" BASIS,
+; WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+; See the License for the specific language governing permissions and
+; limitations under the License.
+; 
+; This file is part of an unsupported extension to Alfresco.
+;
+
+(ns alfresco-technical-validation.indexer
+  (:require [clojure.string        :as s]
+            [clojure.tools.logging :as log]
+            [clojure.java.io       :as io]
+            [clojure.set           :as set]
+            [depends.reader        :as dr]
+            [depends.neo4jwriter   :as dn]
+            [multigrep.core        :as mg])
+  (:use [alfresco-technical-validation.source-indexes]))
+
+(defn- build-file-type-index
+  [files file-type file-regex]
+  { file-type (filter #(re-matches file-regex (.getName ^java.io.File %)) files) })
+
+(defn- build-file-types-index
+  [files]
+  (into {} (map #(build-file-type-index files (key %) (val %)) file-types)))
+
+(defn- build-content-index-for-file-type
+  [file-type file-index]
+  (let [relevant-files   (file-type file-index)
+        relevant-regexes (vals (file-type content-regexes-by-file-type))
+        regex-id-lookup  (set/map-invert (file-type content-regexes-by-file-type))
+        raw-grep-result  (mg/multigrep-files relevant-regexes relevant-files)]
+    (flatten (map #(assoc % :regex-id (get regex-id-lookup (:regex %))) raw-grep-result))))
+
+(defn- build-content-index
+  [file-index]
+  (flatten (map #(build-content-index-for-file-type % file-index) (keys content-regexes-by-file-type))))
+
+(defn index-source
+  "Indexes the sources in the given location and returns an in-memory index."
+  [source]
+  (let [source-files         (file-seq (io/file source))
+        source-files-by-type (build-file-types-index source-files)
+        source-content-index (build-content-index source-files-by-type)]
+    { :source-files-by-type source-files-by-type
+      :source-content-index source-content-index }))
+
+(defn index-binaries
+  "Indexes the binaries in the given location to the specified Neo4J server."
+  [neo4j-url binaries]
+  (let [classes-info (dr/classes-info binaries)]
+    (dn/write-dependencies! neo4j-url classes-info)))
+
+(defn index
+  "Indexes the binaries and sources, returning the in-memory source index."
+  [neo4j-url binaries source]
+  (index-binaries neo4j-url binaries)
+  (index-source source))
+
---
+++ src/alfresco_technical_validation/source_indexes.clj
@@ -0,0 +1,78 @@
+;
+; Copyright © 2013,2014 Peter Monks (pmonks@gmail.com)
+;
+; Licensed under the Apache License, Version 2.0 (the "License");
+; you may not use this file except in compliance with the License.
+; You may obtain a copy of the License at
+; 
+;     http://www.apache.org/licenses/LICENSE-2.0
+; 
+; Unless required by applicable law or agreed to in writing, software
+; distributed under the License is distributed on an "AS IS" BASIS,
+; WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+; See the License for the specific language governing permissions and
+; limitations under the License.
+; 
+; This file is part of an unsupported extension to Alfresco.
+;
+
+(ns alfresco-technical-validation.source-indexes)
+
+; We put these regexes in their own file because Sublime Text barfs on the embedded double quote characters
+; and we lose all of syntax highlighting goodness.
+
+(def file-types
+  "The file types we're interested in, with symbolic names and filename regex to identify them."
+  {
+    :module-properties     #"module\.properties"
+    :java                  #".*\.java"
+    :javascript            #".*\.js"
+    :freemarker            #".*\.ftl"
+    :xml                   #".*\.xml"
+    :web-script-descriptor #".*\.desc\.xml"
+    :spring-app-context    #".*-context\.xml"
+    :content-model         #".*[mM]odel.*\.xml"
+    :explorer-config       #"web-client-config-custom\.xml"
+    :ant                   #"build\.xml"
+    :maven                 #"pom\.xml"
+    :gradle                #"build\.gradle"
+    :leiningen             #"project\.clj"
+    :sbt                   #"build\.sbt"
+    :make                  #"[mM]akefile"
+    :pants                 #"BUILD"
+  })
+
+(def content-regexes-by-file-type
+  "Regexes we want to run over each file type."
+  {
+    :module-properties {
+        :module-version #"module\.version\s*=(.*)\z"
+        :com03          #"module\.id\s*=(.*)\z"
+        :up03-min       #"module\.repo\.version\.min\s*=(.*)\z"
+        :up03-max       #"module\.repo\.version\.max\s*=(.*)\z"
+        :up04           #"module\.editions\s*=(.*)\z"
+      }
+    :java {
+        :stb08-stb09    #"(?:^|\s)synchronized(?:\s|$)"
+      }
+    :javascript {
+        :sec05          #"(?:^|\s)eval\("
+      }
+    :web-script-descriptor {
+        :stb19          #"<transaction>\s*none\s*</transaction>"
+        :stb20          #"<transaction>"
+        :sec03          #"<authentication>\s*none\s*</authentication>"
+      }
+    :spring-app-context {
+        :api05          #"(?:^|\s)ref="
+      }
+    :content-model {
+        :perf02         #"<index\s+enabled\s*=\s*\"true"
+        :perf03         #"<stored>\s*true\s*</stored>"
+        :com08          #"<namespace\s+uri\s*=\s*\".*\"\s+prefix\s*=\s*\"(.*)\"\s*/>"
+        :stb15          #"parent=\"dictionaryModelBootstrap\""
+      }
+    :ant {
+        :ivy            #"antlib:org\.apache\.ivy\.ant"
+      }
+  })
