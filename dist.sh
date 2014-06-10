f76f68d77b518ee92db3f587b48426305818d3b5
Peter Monks <pmonks@gmail.com>
2014-06-09 23:42:03 -0700

    Rolled version to 0.3.0

--- dist.sh
+++ dist.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-VERSION=0.3.0-SNAPSHOT
+VERSION=0.3.0
 
 lein clean && lein check && lein bin
 rc=$?
--- project.clj
+++ project.clj
@@ -16,7 +16,7 @@
 ; This file is part of an unsupported extension to Alfresco.
 ;
 
-(defproject alfresco-technical-validation "0.3.0-SNAPSHOT"
+(defproject alfresco-technical-validation "0.3.0"
   :description      "Performs technical validation of an Alfresco extension."
   :url              "https://github.com/pmonks/depends"
   :license          {:name "Apache License, Version 2.0"
